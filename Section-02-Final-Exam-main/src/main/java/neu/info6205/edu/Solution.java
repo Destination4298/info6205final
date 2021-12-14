c35f8d7d9e5cc2f64b7c594eba04e572929996e5
Destination4298 <zhang.fengy@northeastern.edu>
2021-12-14 10:59:32 -0500

    final exam

--- Section-02-Final-Exam-main/src/main/java/neu/info6205/edu/Solution.java
+++ Section-02-Final-Exam-main/src/main/java/neu/info6205/edu/Solution.java
@@ -2,57 +2,55 @@ package neu.info6205.edu;
 import java.util.List;
 
 public class Solution {
-	
-	
-	private int connectedSquareSum(int graph_nodes, List<Integer> graph_from, List<Integer> graph_to){
-		int result = 0;
-        int temp = 0;
-        int i, j;
+    public int connectedSquareSum(int graph_nodes, List<Integer> graph_from, List<Integer> graph_to){
+
+    	int result = 0;
+        int count = 0;
+        int i = 0;
+        int j = 0;
         int[] visited = new int[graph_nodes];
         int[] head = new int[graph_nodes];
+        
+        
+        
         for (i = 0; i < graph_nodes; i++) {
             head[i] = i;
         }
-        boolean flag = true;
-        while (flag) {
-            flag = false;
+        boolean execute = true;
+        
+        
+        
+        
+        while (execute) {
+        	execute = false;
             for (i = 0; i < graph_from.size(); i++) {
             if (head[graph_from.get(i)] < head[graph_to.get(i)]) {
                 head[graph_to.get(i)] = head[graph_from.get(i)];
-                flag = true;
+                execute = true;
             }
+            
+            
             else if (head[graph_from.get(i)] > head[graph_to.get(i)]) {
+            	
                 head[graph_from.get(i)] = head[graph_to.get(i)];
-                flag = true;
+                execute = true;
             }
             }
         }
         
         for (i = 0; i < graph_nodes; i++) {
             if (head[i] == i) {
-                temp = 0;
+            	count = 0;
                 for (j = 0; j < graph_nodes; j++) {
                     if (head[i] == head[j]) {
-                        temp++;
+                    	count++;
                     }
                 }
-                result += (temp * temp);
+                result += (count * count);
             }
         }
 
         return result;
-    	
-    
-    
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    }
+
 }
